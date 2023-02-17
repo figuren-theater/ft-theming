352e7dfe825e3c84d7a0815a3f1b125d24d17f19
carstenbach <mail@carsten-bach.de>
2023-02-17 09:04:23 +0100

    Bump version and fixed desc. for consistency

--- plugin.php
+++ plugin.php
@@ -2,12 +2,12 @@
 /**
  * Plugin Name:     figuren.theater | Theming
  * Plugin URI:      https://github.com/figuren-theater/ft-theming
- * Description:     This package helps with a consistent look across the figuren.theater Multisite Network and beyond.
+ * Description:     This package helps you & your brand with a consistent look across the figuren.theater Multisite Network and beyond.
  * Author:          figuren.theater
  * Author URI:      https://figuren.theater
  * Text Domain:     figurentheater
  * Domain Path:     /languages
- * Version:         1.0.8
+ * Version:         1.0.9
  *
  * @package         figuren-theater/theming
  */
